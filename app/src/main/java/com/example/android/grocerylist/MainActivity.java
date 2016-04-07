5d3ad883e672ba1fd4224b5040aac0f8079f1494
Olha Tymoshenko <olga.galiba@gmail.com>
2016-04-07 22:41:54 +0300

    Remove menu settings

--- app/src/main/java/com/example/android/grocerylist/MainActivity.java
+++ app/src/main/java/com/example/android/grocerylist/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -47,28 +45,6 @@ public class MainActivity extends AppCompatActivity
         assert listView != null;
         listView.setAdapter(adapter);
         getLoaderManager().initLoader(0, null, this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
