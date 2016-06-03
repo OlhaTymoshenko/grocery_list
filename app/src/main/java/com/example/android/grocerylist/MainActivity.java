842b4174d67325680c22b732e6778d900a8425f3
Olha Tymoshenko <olga.galiba@gmail.com>
2016-06-03 10:49:08 +0300

    Fix FacebookSdk

--- app/src/main/java/com/example/android/grocerylist/MainActivity.java
+++ app/src/main/java/com/example/android/grocerylist/MainActivity.java
@@ -23,7 +23,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 
 import java.util.ArrayList;
@@ -161,7 +160,6 @@ public class MainActivity extends AppCompatActivity
         SharedPreferences.Editor editor = preferences.edit();
         editor.remove("token");
         editor.apply();
-        FacebookSdk.sdkInitialize(getApplicationContext());
         LoginManager.getInstance().logOut();
         SqlRepository repository = new SqlRepository(getApplicationContext());
         repository.logout();
